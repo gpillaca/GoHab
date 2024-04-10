6f5200f89c480392398685ee7d9cee0ced74b537
Geferson PG <gpillacag@gmail.com>
2024-04-10 15:35:46 -0500

    feat: create onboarding screen

---
+++ app/src/main/java/com/gpillaca/gohab/core/components/CustomTitle.kt
@@ -0,0 +1,19 @@
+package com.gpillaca.gohab.core.components
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun CustomTitle(title: String) {
+    Text(
+        text = title.uppercase(),
+        style = MaterialTheme.typography.headlineSmall.copy(
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.tertiary
+        ),
+        textAlign = TextAlign.Center
+    )
+}
