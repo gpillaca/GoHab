d3bb3999a52250a5f81218ff9a89053b04804ab0
Geferson PG <gpillacag@gmail.com>
2024-10-17 14:02:12 -0500

    Add FloatingActionButtonComponent

--- app/src/main/java/pe/geff/gohab/home/HomeScreen.kt
+++ app/src/main/java/pe/geff/gohab/home/HomeScreen.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -26,42 +29,73 @@ fun HomeScreen() {
     HomeScreenContent()
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenContent() {
 
-    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-        CenterAlignedTopAppBar(
-            title = {
-                Text(text = "Home", fontWeight = FontWeight.Bold)
-            },
-            navigationIcon = {
-                Icon(
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .clickable {
-                            // TODO Navigate to SettingsScreen
-                        }
-                        .background(
-                            color = MaterialTheme.colorScheme.inversePrimary,
-                            shape = CircleShape
-                        )
-                        .padding(10.dp),
-                    imageVector = Icons.Default.Settings,
-                    contentDescription = "Settings"
-                )
+    Scaffold(
+        modifier = Modifier.fillMaxSize(), topBar = {
+            TopBarComponent(onClick = {
+                    // TODO Navigate to SettingsScreen
+            })
+        },
+        floatingActionButton = {
+            FloatingActionButtonComponent(onClick = {
+                // TODO Navigate to AddNewHabit
+            })
+        },
+        content = { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+
             }
-        )
-    }) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-
         }
-    }
+    )
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBarComponent(onClick: () -> Unit) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(text = "Home", fontWeight = FontWeight.Bold)
+        },
+        navigationIcon = {
+            Icon(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .clickable(onClick = onClick)
+                    .background(
+                        color = MaterialTheme.colorScheme.inversePrimary,
+                        shape = CircleShape
+                    )
+                    .padding(10.dp),
+                imageVector = Icons.Default.Settings,
+                contentDescription = "Settings"
+            )
+        }
+    )
+}
+
+@Composable
+fun FloatingActionButtonComponent(onClick: () -> Unit) {
+    FloatingActionButton(
+        onClick = onClick,
+        content = {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+        },
+        shape = CircleShape,
+        elevation = FloatingActionButtonDefaults.elevation(defaultElevation = 0.dp),
+        modifier = Modifier
+            .background(
+                color = MaterialTheme.colorScheme.primaryContainer.copy(
+                    alpha = 0.5f
+                ), shape = CircleShape
+            )
+            .padding(8.dp)
+    )
 }
 
 @Preview(showBackground = true)
